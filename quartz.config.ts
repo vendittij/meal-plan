fda0ba19ab8a0cc030aaeeac2cecd436a7b48f7f
John Venditti <jdv005@bucknell.edu>
2025-11-15 19:09:03 -0800

    Quartz sync: Nov 15, 2025, 7:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,10 +8,10 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Meal Plan Recipe Book",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: f,
     analytics: {
       provider: "plausible",
     },
@@ -40,13 +40,13 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
+          light: "#2C061F",
+          lightgray: "#374045",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
+          secondary: "#D89216",
+          tertiary: "#E1D89F",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
